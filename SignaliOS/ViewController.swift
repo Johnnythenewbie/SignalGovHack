135a3139773a35b20d26d9c4cf74859e16fb62df
hanisa1 <hanisamohamed1@gmail.com>
2020-08-15 17:55:12 +1000

    Initial Commit

---
+++ SignaliOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignaliOS
+//
+//  Created by Hanisa Mohamed on 15/8/20.
+//  Copyright © 2020 LitSquad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .magenta
+    }
+
+
+}
+
